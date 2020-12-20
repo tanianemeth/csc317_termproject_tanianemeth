f1d1f75b3d6c698f852f113e61c7ca74026aa96f
tanianemeth <tnemeth@pipeline.sbcc.edu>
2020-12-20 14:38:34 -0700

    now image carousel on homepage displays random images that have been upload by site users

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../database');
+var default_images = [
+  {filename:"1608101838113.jpg"}, {filename: "1608101319270.jpg"}, {filename: "1608255003943.jpg"}
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('session:', req.session);
-  res.render("index", {
-    test: "another bug",
-    session: req.session,
-  });
+  connection.query("SELECT * FROM images ORDER BY RAND() LIMIT 3", function(error, results){
+    var images = results;
+    if (images.length < 3){
+      var num_needed = 3 - images.length
+      for (var i=0; i < num_needed; i++){
+        images.push(default_images[i]);
+      }
+    }
+    console.log(images);
+
+    res.render("index", {
+      session: req.session,
+      images:images,
+    });
+  })
+  
 });
 
 
